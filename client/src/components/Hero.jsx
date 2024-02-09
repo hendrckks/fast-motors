d79dc162ff6585a2067ffd0d88fcd6f6d32d6e4f
hendrckks <samueltetenga@gmail.com>
2024-02-09 11:52:19 +0300

    code transfer

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 const Hero = () => {
   return (
     <div className="w-full h-full">
-      <div className="flex text-2xl text-[#b8c8dd] items-center justify-center">
+      <div className="flex text-3xl text-[#b8c8dd] items-center justify-center">
         <h1 className="mt-16">Find Your New Patner😉</h1>
       </div>
       <div className="flex justify-center items-center mt-16">
-        <ul className="flex gap-5 font-semibold">
+        <ul className="flex gap-5 font-semibold caret-transparent">
           <li>
             <Link
               className="px-14 hover:bg-[#c0ec60] p-4 rounded-sm text-[16px] duration-300 ease-in-out text-[#b8c8dd] hover:text-black"
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,13 +5,13 @@ const Navbar = () => {
   const location = useLocation();
 
   return (
-    <div className="flex font-sans font-[600] bg-[#112744] text-[15px] h-[100px]">
+    <div className="sticky flex border-b-[1px] border-neutral-700 font-sans font-[600] bg-[#112744] text-[15px] h-[100px]">
       <div className={`mt-4 ${location.pathname === "/"}`}>
         <Link to="/">
           <img src={FASTCARR} alt="logo" />
         </Link>
       </div>
-      <div className="p-10 absolute right-0 text-white">
+      <div className="p-10 absolute right-0 text-neutral-200">
         <ul className="flex items-center justify-between">
           <li className="ml-1">
             <Link
