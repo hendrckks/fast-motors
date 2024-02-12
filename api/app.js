e3599dc9f18d6cf7d85e1d5295ae7d0bf66c0d2e
hendrckks <samueltetenga@gmail.com>
2024-02-12 15:14:49 +0300

    Controller functions

--- api/app.js
+++ api/app.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
+
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
@@ -20,3 +22,5 @@ app.listen(PORT, () => {
     console.log("Server is running on port 5173...");
   }
 });
+
+app.use("/api/user", userRouter);
