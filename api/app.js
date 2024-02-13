7f374f3a43de636479b56884707cad8f0932956e
hendrckks <samueltetenga@gmail.com>
2024-02-13 14:05:20 +0300

    axios setup and auth creating auth api

--- api/app.js
+++ api/app.js
@@ -2,10 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
+import axios from "axios";
+const app = express();
 
 dotenv.config();
 const PORT = process.env.port || 5173;
-const app = express();
 
 mongoose
   .connect(process.env.MONGO)
@@ -21,5 +24,5 @@ app.listen(PORT, () => {
     console.log("Server is running on port 5173...");
   }
 });
-
-app.use("/user", userRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
