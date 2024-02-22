3953b971ec744ccd87d13b57bde6f9c0b30f45d7
hendrckks <samueltetenga@gmail.com>
2024-02-22 14:25:07 +0300

    form separation

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import SignupForm from "../components/SignupForm";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({});
@@ -42,49 +43,11 @@ const SignUp = () => {
   console.log(formData);
 
   return (
-    <div className="h-[100vh] flex justify-center">
-      <div className="flex flex-col mt-10 max-w-lg">
-        <h2 className="text-center text-4xl text-neutral-200 my-8">Sign Up</h2>
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col gap-4 text-white"
-        >
-          <input
-            type="text"
-            className="inputs pl-4 border border-transparent focus:outline-none focus:border-transparent focus:ring-0 self-center"
-            placeholder="    Username"
-            onChange={handleChange}
-            id="username"
-          />
-          <input
-            type="username"
-            className="inputs pl-4 border border-transparent focus:outline-none focus:border-transparent focus:ring-0 self-center"
-            placeholder="    Email"
-            onChange={handleChange}
-            id="email"
-          />
-          <input
-            type="password"
-            className="inputs pl-4 border border-transparent focus:outline-none focus:border-transparent focus:ring-0 self-center"
-            placeholder="    Password"
-            onChange={handleChange}
-            id="password"
-          />
-          <button
-            disabled={loading}
-            className="my-8 rounded-md bg-[#c0ec60] text-black font-[600] h-[50px] w-1/2 p-2 justify-center text-[14px] hover:scale-105 ease-in-out duration-200 border border-transparent focus:outline-none focus:border-transparent focus:ring-0 self-center" // Added self-center class
-          >
-            {loading ? "LOADING..." : "SIGN UP"}
-          </button>
-        </form>
-        <div className="flex justify-center">
-          <p className="text-neutral-500">Have an account?</p>
-          <Link className="ml-2 text-neutral-200" to="/login">
-            LogIn
-          </Link>
-        </div>
-      </div>
-    </div>
+    <SignupForm
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+      loading={loading}
+    />
   );
 };
 
