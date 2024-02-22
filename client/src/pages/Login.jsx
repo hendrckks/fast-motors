b2a9f6832258e90b238d3adea9d079baf040e0b5
hendrckks <samueltetenga@gmail.com>
2024-02-22 14:24:09 +0300

    redux persist

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -7,12 +7,14 @@ import {
   logInFailure,
   logInSuccess,
 } from "../redux/user/userSlice.js";
+import LoginForm from "../components/LoginForm.jsx";
 
 const Login = () => {
   const dispatch = useDispatch();
-  const [formData, setFormData] = useState({});
-  const { loading, error } = useSelector((state) => state.User);
   const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -22,13 +24,6 @@ const Login = () => {
     e.preventDefault();
     try {
       dispatch(logInStart());
-      // const res = await fetch("http://localhost:3000/api/auth/signup", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify(formData),
-      // });
       const res = await axios.post(
         "http://localhost:3000/auth/login",
         formData,
@@ -41,7 +36,7 @@ const Login = () => {
       const data = res.data;
       if (data.success === false) {
         dispatch(logInFailure(data.message));
-        return;
+        return console.log(data.message);
       }
       dispatch(logInSuccess(data));
       navigate("/");
@@ -49,46 +44,14 @@ const Login = () => {
       dispatch(logInFailure(error.message));
     }
   };
-  console.log(formData);
 
   return (
-    <div className="h-[100vh] flex justify-center">
-      <div className="flex flex-col mt-10 max-w-lg">
-        <h2 className="text-center text-4xl text-neutral-200 my-8">Login</h2>
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col gap-4 text-white"
-        >
-          <input
-            type="username"
-            className="inputs pl-4 border border-transparent focus:outline-none focus:border-transparent focus:ring-0 self-center"
-            placeholder="    Email"
-            onChange={handleChange}
-            id="email"
-          />
-          <input
-            type="password"
-            className="inputs pl-4 border border-transparent focus:outline-none focus:border-transparent focus:ring-0 self-center"
-            placeholder="    Password"
-            onChange={handleChange}
-            id="password"
-          />
-          <button
-            disabled={loading}
-            className="my-8 rounded-md bg-[#c0ec60] text-black font-[600] h-[50px] w-1/2 p-2 justify-center text-[14px] hover:scale-105 ease-in-out duration-200 border border-transparent focus:outline-none focus:border-transparent focus:ring-0 self-center" // Added self-center class
-          >
-            {loading ? "LOADING..." : "LOGIN"}
-          </button>
-        </form>
-        <div className="flex justify-center">
-          <p className="text-neutral-500">Dont have an account?</p>
-          <Link className="ml-2 text-neutral-200" to="/signup">
-            Sign In
-          </Link>
-        </div>
-        {error && <p>{error}</p>}
-      </div>
-    </div>
+    <LoginForm
+      error={error}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+      loading={loading}
+    />
   );
 };
 
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,35 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { userReducer } from "./user/userSlice.js";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: { user: userReducer },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serialzableCheck: false,
+      serialzableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
     }),
 });
+
+export const persistor = persistStore(store);
